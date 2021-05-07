26dae93a0918333642bbb6e2f3deca6f38eb802c
ckti <7046769+ckti@users.noreply.github.com>
2021-05-07 10:33:51 +0100

    Add files via upload

--- feature_block_reward_reallocation.py
+++ feature_block_reward_reallocation.py
@@ -25,7 +25,6 @@ class BlockRewardReallocationTest(DashTestFramework):
     # 536870912 == 0x20000000, i.e. not signalling for anything
     def create_test_block(self, version=536870912):
         self.bump_mocktime(5)
-        sleep(1000)
         bt = self.nodes[0].getblocktemplate()
         tip = int(bt['previousblockhash'], 16)
         nextheight = bt['height']
